94eb8a1d10b6aa7cdac485cfa43fbc2cb02ae988
alisawiali <alisawiali70@gmail.com>
2024-04-03 15:23:43 +0200

    Update footer page und mit Responsev erstellt

--- frontend/src/compoents/footer/Footer.jsx
+++ frontend/src/compoents/footer/Footer.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="w-full bg-gray-900 text-white mt-8 px-2 md:px-[200px] flex md:flex-row flex-col text-center space-y-4 md:space-y-0  md:justify-between text-sm md:text-md py-8 md:mt-8">
+        <div className="flex flex-col text-white">
+          <p>Bibliothek</p>
+          <p>Meist gelesen</p>
+          <p>Empfohlene Blogs</p>
+        </div>
+        <div className="flex flex-col text-white">
+          <p>Forum</p>
+          <p>Suport </p>
+          <p>Recent Posts</p>
+        </div>
+
+        <div className="flex flex-col text-white">
+          <p>Privacy</p>
+          <p>About Us </p>
+          <p>Terms Conditions</p>
+          <p>Terms of Service</p>
+        </div>
+      </div>
+
+      <p className="text-center bg-gray-900 text-white pb-4  py-4 text-sm">
+        All rights reserved &copy; Bibliothek Markt 2024
+      </p>
+    </>
+  );
 };
 
 export default Footer;
--- frontend/src/compoents/homepage/HomePosts.jsx
+++ frontend/src/compoents/homepage/HomePosts.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import imgBook from "../../assets/books.png";
 const HomePosts = () => {
   return (
-    <div className="w-full flex mt-8 space-x-2 ">
+    <div className="w-full flex mt-8 space-x-2 flex-col md:flex-row myt-8  space-y-4 md:space-y-0 md:space-x-2">
       {/* left */}
-      <div className="flex w-[35%] h-[200px] jsutify-center items-center">
+      <div className="flex md:w-1/3 h-[200px] justify-center items-center">
         <img src={imgBook} alt="" className="h-full w-full" />
       </div>
       {/* right */}
-      <div className="flex flex-col w-[65%]">
+      <div className="flex flex-col md:w-2/3 ">
         <h1 className="text-xl font-bold md:mb-2 mb-1 mb:text-2x1 ">
           10 Uses of artifcial intelliganze in Day to Day Life
         </h1>
@@ -19,7 +19,7 @@ const HomePosts = () => {
             <p>23:41</p>
           </div>
         </div>
-        <p className="text-sm md:text-x1 my-4">
+        <p className="text-sm  my-4">
           Prominente Beispiele für KI-Software im täglichen Leben sind
           Sprachassistenten, Bilderkennung, Empfehlungssysteme, persönliche
           Assistenten, Übersetzungswerkzeuge, Chatbots, Gesichtserkennung und
