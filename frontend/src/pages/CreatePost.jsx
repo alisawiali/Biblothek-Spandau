1007f314848fbfe46cdeafad83e5d8ff27a32b43
alisawiali <alisawiali70@gmail.com>
2024-04-04 12:59:25 +0200

    Untl styled

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -6,9 +6,10 @@ const CreatePost = () => {
   return (
     <div>
       <Navbar />
-      <div className="px-6 md:px-[200px] mb-8 ">
+      {/*   <div className=" mx-auto mt-20 bg-gray-200 p-4"> */}
+      <div className=" px-6 md:px-[200px] mt-20 ">
         <h1 className="font-bold md:text-2xl text-xl">Create a Post</h1>
-        <form className="flex flex-col px-4 py-2 outline-none mt-4">
+        <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4  ">
           <input
             type="text"
             placeholder="Enter a title"
@@ -22,7 +23,7 @@ const CreatePost = () => {
                 className="px-4 py-2 outline-none"
                 placeholder="Enter post category "
               />
-              <div className="bg-black text-white px-4 py-2 font-semibold cursor-pointer rounded-2xl">
+              <div className="bg-black text-white px-4 py-2 font-semibold cursor-pointer rounded-sm">
                 Add
               </div>
             </div>
@@ -42,6 +43,15 @@ const CreatePost = () => {
               </div>
             </div>
           </div>
+          <textarea
+            roes={15}
+            cols={30}
+            className="px-4 py-2 outline-none"
+            placeholder="Enter Your Description"
+          />
+          <button className="bg-black w-full md:w-[20%] mx-auto text-white  font-semibold px-4 py-2 md:text-xl text-lg rounded-sm">
+            create
+          </button>
         </form>
       </div>
       <Footer />
