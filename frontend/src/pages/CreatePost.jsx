abb755f7c99cc3a65bc94710e080226c32b39f76
alisawiali <alisawiali70@gmail.com>
2024-04-04 13:42:21 +0200

    Hooks (useState) hinzugefügt, um den Eingabewert der Kategorie (cat) und die Liste der Kategorien (cats) zu verwalten. Außerdem wurde eine Funktion addCategory() erstellt, um neue Kategorien hinzuzufügen, sowie eine leere Funktion deletCategroy() für das Löschen von Kategorien.

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer, Navbar } from "../compoents/UL";
 // Import icons aud dem reat icons
 import { ImCross } from "react-icons/im";
+
+//  Exporting  CreatePost
 const CreatePost = () => {
+  //  Hocks
+  const [cat, setCat] = useState("");
+  const [cats, setCats] = useState([]);
+
+  const deletCategroy = () => {};
+  //
+  const addCategory = () => {
+    let updateCats = [...cats];
+    updateCats.push(cat);
+    setCat("");
+    setCats(updateCats);
+  };
+
   return (
     <div>
       <Navbar />
-      {/*   <div className=" mx-auto mt-20 bg-gray-200 p-4"> */}
       <div className=" px-6 md:px-[200px] mt-20 ">
         <h1 className="font-bold md:text-2xl text-xl">Create a Post</h1>
         <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4  ">
@@ -22,25 +36,32 @@ const CreatePost = () => {
                 type="text"
                 className="px-4 py-2 outline-none"
                 placeholder="Enter post category "
+                value={cat}
+                onChange={(e) => setCat(e.target.value)}
               />
-              <div className="bg-black text-white px-4 py-2 font-semibold cursor-pointer rounded-sm">
+              <div
+                onClick={addCategory}
+                className="bg-black text-white px-4 py-2 font-semibold cursor-pointer rounded-sm"
+              >
                 Add
               </div>
             </div>
             {/* categories */}
             <div className="flex px-4 mt-3">
-              <div className="flex justify-center items-center space-x-2 bg-gray-200 px-2 py-1 rounded-md m-1">
-                <p>Tach</p>
-                <p className="text-white bg-black rounded-full cursor-pointer p-1 text-sm ">
-                  <ImCross />
-                </p>
-              </div>
-              <div className="flex justify-center items-center space-x-2 bg-gray-200 px-2 py-1 rounded-md m-1">
-                <p>Tach</p>
-                <p className="text-white bg-black rounded-full cursor-pointer p-1 text-sm">
-                  <ImCross />
-                </p>
-              </div>
+              {cats?.map((items, id) => (
+                <div
+                  key={id}
+                  className="flex justify-center items-center space-x-2 bg-gray-200 px-2 py-1 rounded-md m-1"
+                >
+                  <p>{items}</p>
+                  <p
+                    onClick={deletCategroy}
+                    className="text-white bg-black rounded-full cursor-pointer p-1 text-sm "
+                  >
+                    <ImCross />
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
           <textarea
