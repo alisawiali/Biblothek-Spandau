b277ebe38624941a92ef92a250fccca2bbe885c8
alisawiali <alisawiali70@gmail.com>
2024-04-05 12:13:44 +0200

    installiert cors mongoose bcrypt axioscookie-parser dotenv express  multer jsonwebtoken und ich habe index.js und .env auf 1986 erstellt

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
