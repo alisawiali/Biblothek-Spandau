428358b5ccf220b1a6a03bc628c4f0110b131f28
alisawiali <alisawiali70@gmail.com>
2024-04-08 12:59:34 +0200

    Update get userId und wenn den user nicht mehr stattgefunden dann kebommt ein error  message, dass  das Username nicht gefunden ist

--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -18,8 +18,14 @@ export const updateUser = async (req, res, next) => {
       { $set: req.body },
       { new: true }
     );
+    if (!updatedUser) {
+      res.status(409).send("User  nicht exists");
+    }
     res.status(200).send(updatedUser);
   } catch (error) {
+    if (error.code === 11000) {
+      res.status(409).send(error.message);
+    }
     next(error);
   }
 };
@@ -34,21 +40,22 @@ export const deletedUser = async (req, res, next) => {
 
     res.status(200).json("post and user successfully deleted");
   } catch (error) {
-    if (error.code === 11000) {
-      return res.status(409).json({
-        success: true,
-        message: "Benutzername bereits deleted",
-      });
-    }
     next(error.message);
   }
 };
 
 // //GET USER
-// export const getUser = (req, res, next) => {
-//   try {
-//   } catch (error) {}
-// };
+export const getUserId = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      res.status(409).send("User not found");
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // // GET IDUSER
 // export const getUserId = (req, res, next) => {
--- backend/src/router/user.js
+++ backend/src/router/user.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { deletedUser, updateUser } from "../controller/user.js";
+import { deletedUser, getUserId, updateUser } from "../controller/user.js";
 const router = Router();
 
 //
 router.put("/:id", updateUser);
 router.delete("/:id", deletedUser);
 router.get("/");
-router.get("/:id");
+router.get("/:id", getUserId);
 router.post("/");
 
 export default router;
