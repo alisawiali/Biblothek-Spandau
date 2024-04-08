f5446334dc6c5adafbe8e01636e7dc3b1ebc98d5
alisawiali <alisawiali70@gmail.com>
2024-04-08 16:19:34 +0200

    Update middelware function

---
+++ backend/src/middlewares/verifyToken.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const verfiyToken = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).send("Your are not authenticated to access");
+    }
+    jwt.verify(token, process.env.SECRET_KEY, async (err, data) => {
+      if (err) {
+        return res.status(403).json("Token is not Vaild"); //vaild => صالح
+      }
+      res.userId = data.id;
+      next();
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
