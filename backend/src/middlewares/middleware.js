a52fe37e6aa0654aacff03bb96cff85576584a1e
alisawiali <alisawiali70@gmail.com>
2024-04-08 18:02:09 +0200

    und wenn den token password nicht richtig ist dann bekommt den user 403 "token nicht vaild""

--- backend/src/middlewares/middleware.js
+++ backend/src/middlewares/middleware.js
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
 
-  console.log(token);
   if (!token) {
     return res.status(401).json("Your are not authenticated to access");
   }
