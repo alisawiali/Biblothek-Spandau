3e753313bdbd9110e0499ad066f915dd6cdd5711
alisawiali <alisawiali70@gmail.com>
2024-04-09 13:54:58 +0200

    import usecontext und UserHockContext im menu

--- frontend/src/compoents/menu/Menu.jsx
+++ frontend/src/compoents/menu/Menu.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { UserHookContext } from "../../context/userContext";
 
 const Menu = () => {
-  const user = false;
+  const { user } = useContext(UserHookContext);
   return (
     <div className="bg-black w-[200px] flex flex-col items-start absolute md:right-32 top-12 right-6 p-4 rounded-md space-y-4">
       {!user && (
         <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">
-          Login
+          <NavLink to="/login"> Login</NavLink>
         </h3>
       )}
       {!user && (
         <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">
-          Register
+          <NavLink to="/register"> Register</NavLink>
         </h3>
       )}
       {user && (
