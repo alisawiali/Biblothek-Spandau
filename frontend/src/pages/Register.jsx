c1b4eff1afa86c01212e0d77e21bdc279c7eab69
alisawiali <alisawiali70@gmail.com>
2024-04-09 11:26:04 +0200

    axios Methode: Um HTTP-Anfragen an einen Server zu senden und Daten von APIs abzurufen das ist for fronend und für backend,  um HTTP-Anfragen an andere Server oder APIs zu senden

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Footer } from "../compoents/UL";
+import axios from "axios";
+import { URL } from "../url";
+//
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  //
+  const handelRegister = async () => {
+    try {
+      const res = await axios.post(URL + "/api/auth/register");
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //
   return (
     <>
       <div className="flex items-center justify-between px-6 py-4 md:px-[200px] ">
@@ -14,21 +32,27 @@ const Register = () => {
         <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]">
           <h1 className="text-xl font-bold text-left">Create an Aaccount</h1>
           <input
+            onChange={(e) => setUsername(e.target.value)}
             className="w-full font-black-400  border-2 border-black outline-0 px-1 py-2"
             type="text"
             placeholder="Enter your username "
           />
           <input
+            onChange={(e) => setEmail(e.target.value)}
             className="w-full font-black-400 border-2 border-black outline-0 px-1 py-2"
             type="text"
             placeholder="Enter your email"
           />
           <input
+            onChange={(e) => setPassword(e.target.value)}
             className="w-full font-black-400  border-2 border-black outline-0 px-1 py-2"
             type="password"
             placeholder="Enter your password "
           />
-          <button className="w-full bg-gray-900 text-white py-2 px-4 font-bold rounded-lg hover:bg-gray-700 transition-all duration-500 ease  cursor-pointer">
+          <button
+            onClick={handelRegister}
+            className="w-full bg-gray-900 text-white py-2 px-4 font-bold rounded-lg hover:bg-gray-700 transition-all duration-500 ease  cursor-pointer"
+          >
             Register
           </button>
 
