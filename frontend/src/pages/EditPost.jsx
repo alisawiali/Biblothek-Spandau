d70894e2ae4b640a19b6eb1eefb9a0eeff034f39
alisawiali <alisawiali70@gmail.com>
2024-04-12 16:32:48 +0200

    Implementierung der Funktion zum Abrufen von Post-Daten,In dieser Änderung wurde die Funktion fetchPost hinzugefügt, die verwendet wird, um Post-Daten von der API abzurufen und sie in den entsprechenden Hooks zu speichern. Änderungen:- Hinzugefügt: fetchPost-Funktion zum Abrufen von Post-Daten. - Verwendung von axios.get für den API-Aufruf.- Verwendung von Hooks (useState) zum Aktualisieren von Title, Description und File.Details: - Die fetchPost-Funktion verwendet axios, um Daten von der API abzurufen, und aktualisiert dann die State-Hooks mit den erhaltenen Daten. - Titel, Beschreibung und Datei werden basierend auf den erhaltenen Daten aktualisiert, um die Benutzeroberfläche entsprechend zu aktualisieren. - Fehlerbehandlung wurde implementiert, um etwaige Fehler beim Abrufen der Daten zu behandeln und in der Konsole auszugeben.Implementierung der Funktion zum Abrufen von Post-Daten

--- frontend/src/pages/EditPost.jsx
+++ frontend/src/pages/EditPost.jsx
@@ -2,8 +2,17 @@ import React, { useState } from "react";
 import { Navbar, Footer } from "../compoents/UL";
 // Import icons aud dem reat icons
 import { ImCross } from "react-icons/im";
+import axios from "axios";
+import { URL } from "../url";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 const EditPost = () => {
+  //
+  const postId = useParams().id;
   //  Hocks
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [file, setFile] = useState(null);
   const [cat, setCat] = useState("");
   const [cats, setCats] = useState([]);
 
@@ -20,18 +29,43 @@ const EditPost = () => {
     setCat("");
     setCats(updateCats);
   };
+
+  const fetchPost = async () => {
+    try {
+      const res = await axios.get(`${URL}/api/posts/` + postId);
+      console.log(res.data);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
+      setFile(res.data.photo);
+      setCats(res.data.categories);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, [postId]);
+
+  //
   return (
     <div>
       <Navbar />
-      <div className=" px-6  md:px-[200px] mt-20  h-[450px]">
+      <div className=" px-6 py-4 my-12 md:px-[200px] mt-20  h-[500px] ">
         <h1 className="font-bold md:text-2xl text-xl">Update a Post</h1>
         <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4  ">
           <input
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
             type="text"
             placeholder="Enter a title"
             className="px-4 py-2 outline-none"
           />
-          <input type="file" className="px-4" />
+          <input
+            onChange={(e) => setFile(e.target.files[0])}
+            type="file"
+            className="px-4"
+          />
           <div className="flex flex-col">
             <div className="flex items-center space-x-4 md:space-x-8">
               <input
@@ -67,12 +101,14 @@ const EditPost = () => {
             </div>
           </div>
           <textarea
+            onChange={(e) => setDesc(e.target.value)}
+            value={desc}
             roes={15}
             cols={30}
-            className="px-4 py-2 outline-none"
+            className="px-4 py-2 outline-none h-[20vh] "
             placeholder="Enter Your Description"
           />
-          <button className="bg-black w-full md:w-[20%] mx-auto text-white  font-semibold px-4 py-2 md:text-xl text-lg rounded-sm">
+          <button className="bg-black w-full md:w-[20%] mx-auto text-white  font-semibold px-4 py-2 md:text-xl text-lg rounded-sm my-8">
             Update
           </button>
         </form>
