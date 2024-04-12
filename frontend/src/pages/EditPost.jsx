d9beefa90db4147c974d51c301f953e1eb7cd505
alisawiali <alisawiali70@gmail.com>
2024-04-12 17:04:21 +0200

    Update das edifPost

--- frontend/src/pages/EditPost.jsx
+++ frontend/src/pages/EditPost.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Navbar, Footer } from "../compoents/UL";
 // Import icons aud dem reat icons
 import { ImCross } from "react-icons/im";
 import axios from "axios";
 import { URL } from "../url";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
+import { UserHookContext } from "../context/userContext";
 const EditPost = () => {
   //
   const postId = useParams().id;
@@ -15,6 +16,9 @@ const EditPost = () => {
   const [file, setFile] = useState(null);
   const [cat, setCat] = useState("");
   const [cats, setCats] = useState([]);
+  const navigate = useNavigate();
+  //  usecontext
+  const { user } = useContext(UserHookContext);
 
   const deleteCategory = (item) => {
     const updatedCats = cats.filter((c, i) => i !== item);
@@ -22,17 +26,22 @@ const EditPost = () => {
   };
   // Funktion zum Hinzufügen einer Kategorie
   const addCategory = () => {
-    //   setCats((prevCats) => [...prevCats, cat]); // Eine neue Kategorie hinzufügen
-    //   setCat(""); // Kategorie zurücksetzen ODER-------------
     let updateCats = [...cats];
     updateCats.push(cat);
     setCat("");
     setCats(updateCats);
   };
 
+  /**
+   * Fetches a post from the server using the postId, then updates the state
+   * with the retrieved post's data.
+   * @param {string} postId - The ID of the post to fetch.
+   * */
   const fetchPost = async () => {
     try {
+      // Send a GET request to the server to fetch the post data
       const res = await axios.get(`${URL}/api/posts/` + postId);
+      // Aktualisiert den Zustand mit den abgerufenen Postdaten
       console.log(res.data);
       setTitle(res.data.title);
       setDesc(res.data.desc);
@@ -47,11 +56,46 @@ const EditPost = () => {
     fetchPost();
   }, [postId]);
 
+  const handelUpdate = async (e) => {
+    e.preventDefault();
+    const post = {
+      title,
+      desc,
+      username: user.username,
+      userId: user._id,
+      categories: cats,
+    };
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("img", filename);
+      data.append("file", file);
+      post.photo = filename;
+      console.log(data);
+      // Img Upload
+      try {
+        const imgUpload = await axios.post(`${URL}/api/upload`, data);
+        console.log(imgUpload);
+        // console.log(imgUpload);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    //Post Upload
+    try {
+      const res = await axios.put(`${URL}/api/posts/` + postId, post, {
+        withCredentials: true,
+      });
+      navigate("/posts/post/" + res.data._id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   //
   return (
     <div>
       <Navbar />
-      <div className=" px-6 py-4 my-12 md:px-[200px] mt-20  h-[500px] ">
+      <div className=" px-6 py-4 my-20 md:px-[200px] mt-20  h-[500px] ">
         <h1 className="font-bold md:text-2xl text-xl">Update a Post</h1>
         <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4  ">
           <input
@@ -108,7 +152,10 @@ const EditPost = () => {
             className="px-4 py-2 outline-none h-[20vh] "
             placeholder="Enter Your Description"
           />
-          <button className="bg-black w-full md:w-[20%] mx-auto text-white  font-semibold px-4 py-2 md:text-xl text-lg rounded-sm my-8">
+          <button
+            onClick={handelUpdate}
+            className="bg-black w-full md:w-[20%] mx-auto text-white  font-semibold px-4 py-2 md:text-xl text-lg rounded-sm my-8"
+          >
             Update
           </button>
         </form>
